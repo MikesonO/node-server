22f00dc604b2b9975507a820b0f19002cb5deffa
mike.osinowo <mikeson.jnr@gmail.com>
2024-01-03 16:20:07 +0000

    Replicate shop.html with pug template engine

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
 const adminData = require('./routes/admin.js');
 const shopRoutes = require('./routes/shop.js');
 
