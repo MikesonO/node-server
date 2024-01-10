a4e5a30a5cf8fbbe25f3dc675519ff3d8763e6a7
mike.osinowo <mikeson.jnr@gmail.com>
2024-01-10 14:59:19 +0000

    Create Handlebars layout

--- app.js
+++ app.js
@@ -5,14 +5,20 @@ const expressHbs = require('express-handlebars');
 
 const app = express();
 
-app.engine('hbs', expressHbs());
+app.engine('hbs', expressHbs({
+    layoutDir: 'views/layouts/',
+    defaultLayout: 'main-layout',
+    extname: 'hbs'
+}));
 app.set('view engine', 'hbs');
 app.set('views', 'views');
 
 const adminData = require('./routes/admin.js');
 const shopRoutes = require('./routes/shop.js');
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 
@@ -21,9 +27,9 @@ app.use(shopRoutes);
 
 
 app.use((req, res, next) => {
-    res.status(404).render('404', { pageTitle: 'Page Not Found' });
+    res.status(404).render('404', {
+        pageTitle: 'Page Not Found'
+    });
 });
 
 app.listen(3000);
-
-
--- routes/shop.js
+++ routes/shop.js
@@ -2,16 +2,20 @@ const path = require('path');
 
 const express = require('express');
 
-const rootDir = require('../helper_functions/path');
 const adminData = require('./admin');
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    const products = adminData.products;
-    res.render('shop', { pageTitle: 'Shop', prods: products, path: '/', hasProducts: products.length > 0 });
+  const products = adminData.products;
+  res.render('shop', {
+    prods: products,
+    pageTitle: 'Shop',
+    path: '/',
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productCSS: true
+  });
 });
 
 module.exports = router;
-
-
