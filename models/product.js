0e5de3364fbd37231ac544eaa389caf13dd34361
mikeson <mikeson.jnr@gmail.com>
2024-01-13 23:50:44 +0000

    Create product model

---
+++ models/product.js
@@ -0,0 +1,14 @@
+const products = [];
+module.exports = class Product {
+  constructor(title) {
+    this.title = title;
+  }
+
+  saveProduct() {
+    products.push(this);
+  }
+
+  static fetchAll() {
+    return products;
+  }
+}
