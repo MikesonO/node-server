9ed91c2f34e312604f4f80ff74f519fc0ade971a
mikeson <mikeson.jnr@gmail.com>
2024-01-14 00:18:53 +0000

    Store products in products.json file

--- models/product.js
+++ models/product.js
@@ -1,14 +1,36 @@
-const products = [];
+const fs = require('fs');
+const path = require('path');
+
 module.exports = class Product {
   constructor(title) {
     this.title = title;
   }
 
   saveProduct() {
-    products.push(this);
+    const p = path.join(path.dirname(process.mainModule.filename),
+    'data',
+    'products.json');
+    fs.readFile(p, (err, fileContent) => {
+      let products = [];
+      if (!err) {
+        products = JSON.parse(fileContent);
+      }
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), (err) => {
+        console.log(err);
+      });
+    });
   }
 
-  static fetchAll() {
-    return products;
+  static fetchAll(callBack) {
+    const p = path.join(path.dirname(process.mainModule.filename),
+    'data',
+    'products.json');
+    fs.readFile (p, (err, fileContent) => {
+      if (err) {
+        callBack([]);
+      }
+      callBack(JSON.parse(fileContent));
+    })
   }
 }
