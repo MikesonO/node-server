a1660584b20e76cc84b935bb297dcbb3d79f2bc1
mike.osinowo <mikeson.jnr@gmail.com>
2024-02-01 19:02:07 +0000

    Add delete functionality

--- controllers/admin.js
+++ controllers/admin.js
@@ -61,3 +61,9 @@ exports.getProducts = (req, res, next) => {
         });
     });
 };
+
+exports.postDeleteProduct = (req, res, next) => {
+    const prodId = req.body.productId;
+    Product.deleteProduct(prodId);
+    res.redirect('/admin/product-list');
+}
--- models/cart.js
+++ models/cart.js
@@ -11,7 +11,10 @@ module.exports = class Cart {
   static addProduct(id, productPrice) {
     // Fetch the previous cart
     fs.readFile(p, (err, fileContent) => {
-      let cart = { products: [], totalPrice: 0 };
+      let cart = {
+        products: [],
+        totalPrice: 0
+      };
       if (!err) {
         cart = JSON.parse(fileContent);
       }
@@ -23,12 +26,17 @@ module.exports = class Cart {
       let updatedProduct;
       // Add new product/ increase quantity
       if (existingProduct) {
-        updatedProduct = { ...existingProduct };
+        updatedProduct = {
+          ...existingProduct
+        };
         updatedProduct.qty = updatedProduct.qty + 1;
         cart.products = [...cart.products];
         cart.products[existingProductIndex] = updatedProduct;
       } else {
-        updatedProduct = { id: id, qty: 1 };
+        updatedProduct = {
+          id: id,
+          qty: 1
+        };
         cart.products = [...cart.products, updatedProduct];
       }
       cart.totalPrice = cart.totalPrice + +productPrice;
@@ -37,4 +45,26 @@ module.exports = class Cart {
       });
     });
   }
+
+  static deleteProduct(id, productPrice) {
+    fs.readFile(p, (err, fileContent) => {
+      if (err) {
+        return
+      }
+      const updatedCart = { ...JSON.parse(fileContent) };
+      const product = updatedCart.products.findIndex(prod => prod.id === id);
+      const productQuantity = product.quantity;
+
+      updatedCart.products = updatedCart.products.filter(prod => prod !== id);
+
+      updatedCart.totalPrice = updatedCart.totalPrice - productPrice * productQuantity;
+
+      // Store updatedCart
+      fs.writeFile(p, JSON.stringify(updatedCart), err => {
+        console.log(err);
+      });
+
+    });
+  }
+
 };
