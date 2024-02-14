9466674284af935abed4ee83b36d80d962995d3e
mike.osinowo <mikeson.jnr@gmail.com>
2024-02-14 15:29:25 +0000

    Display orders on page

--- models/user.js
+++ models/user.js
@@ -135,10 +135,10 @@ class User {
     }
 
 
-    // getOrders() {
-    //     const db = getDb();
-    //     return db.collection('orders');
-    // }
+    getOrders() {
+        const db = getDb();
+        return db.collection('orders').find({ 'user._id': new ObjectId(this._id) }).toArray();
+    }
 
     static findById(userId) {
         const db = getDb();
--- routes/shop.js
+++ routes/shop.js
@@ -22,13 +22,10 @@ router.post('/cart', shopController.postCart);
 // // Cart Page | /cart-delete-item => POST
 router.post('/cart-delete-item', shopController.postCartDeleteProduct);
 
-// // Checkout Page | /checkout => GET
-// router.get('/checkout', shopController.getCheckout);
-
 // // Orders Page | /orders => POST
 router.post('/create-order', shopController.postOrder);
 
 // // Orders Page | /cart => GET
-// router.get('/orders', shopController.getOrders);
+router.get('/orders', shopController.getOrders);
 
 module.exports = router;
