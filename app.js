8dfe84e69b2128445a8d93a7218dd94359f7a51c
mike.osinowo <mikeson.jnr@gmail.com>
2024-02-20 15:51:40 +0000

    Create user schema

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const errorControllers = require('./controllers/errors.js');
-// const User = require('./models/user.js');
+const User = require('./models/user.js');
 
 const app = express();
 
@@ -20,14 +20,14 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use((req, res, next) => {
-//     User.findById('65c2664413fae1e5fd8a601b')
-//         .then(user => {
-//             req.user = new User(user.name, user.email, user.cart, user._id);
-//             next();
-//         })
-//         .catch(err => console.log(err));
-// });
+app.use((req, res, next) => {
+    User.findById('65d4ca5d060e0df0af6dfb79')
+        .then(user => {
+            req.user = user;
+            next();
+        })
+        .catch(err => console.log(err));
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -37,6 +37,20 @@ app.use(errorControllers.get404);
 
 mongoose.connect('mongodb+srv://Mike:T0wtSsPws1dOPuFL@cluster.8oboqne.mongodb.net/shop?retryWrites=true')
     .then(results => {
+        // If there are no users - create a new one
+        User.findOne().then(user => {
+            if (!user) {
+                const user = new User({
+                    name: 'Mike',
+                    email: 'mike@test.com',
+                    cart: {
+                        items: []
+                    }
+                });
+                user.save();
+            }
+        });
+
         app.listen(3000);
     })
     .catch(err => {
