2baec75f3750e998b339b53ea4b51e94722a0742
mike.osinowo <mikeson.jnr@gmail.com>
2024-03-21 14:59:26 +0000

    Add signin functionality

--- controllers/auth.js
+++ controllers/auth.js
@@ -19,14 +19,30 @@ exports.getSignup = (req, res, next) => {
 };
 
 exports.postLogin = (req, res, next) => {
-    User.findById('65d4ca5d060e0df0af6dfb79')
+    const email = req.body.email;
+    const password = req.body.password;
+
+    User.findOne({ email: email })
         .then(user => {
-            req.session.isLoggedIn = true;
-            req.session.user = user;
-            req.session.save(err => {
+            if (!user) {
+                return res.redirect('/login');
+            }
+            bcrypt.compare(password, user.password).then(doMatch => {
+                if (doMatch) {
+                    req.session.isLoggedIn = true;
+                    req.session.user = user;
+                    return req.session.save(err => {
+                        console.log(err);
+                        res.redirect('/')
+                    });
+                }
+                res.redirect('/login');
+
+            }).catch(err => {
                 console.log(err);
-                res.redirect('/');
+                res.redirect('/login')
             });
+
         })
         .catch(err => console.log(err));
 };
