e43a121bb009e856debf5d2aa31df59b408a97ae
mike.osinowo <mikeson.jnr@gmail.com>
2024-03-25 17:20:02 +0000

    Add conditional CSS classes

--- controllers/auth.js
+++ controllers/auth.js
@@ -45,7 +45,8 @@ exports.getSignup = (req, res, next) => {
             email: '',
             password: '',
             confirmPassword: ''
-        }
+        },
+        validationErrors: []
     });
 };
 
@@ -105,7 +106,8 @@ exports.postSignup = (req, res, next) => {
             path: '/signup',
             pageTitle: 'Signup',
             errorMessage: errors.array()[0].msg,
-            oldInput: { email: email, password: password, confirmPassword: confirmPassword }
+            oldInput: { email: email, password: password, confirmPassword: confirmPassword },
+            validationErrors: errors.array()
         });
     }
 
