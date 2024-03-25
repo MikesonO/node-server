41ff8fb061293470fc118723dfc6b1d119b562c6
mike.osinowo <mikeson.jnr@gmail.com>
2024-03-25 12:46:52 +0000

    Set up validation

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,7 @@
 const crypto = require('crypto');
 const bcrypt = require('bcryptjs');
 const { MailtrapClient } = require("mailtrap");
+const { validationResult } = require('express-validator');
 
 const User = require('../models/user');
 const { reset } = require('nodemon');
@@ -80,6 +81,16 @@ exports.postSignup = (req, res, next) => {
     const email = req.body.email;
     const password = req.body.password;
     const confirmPassword = req.body.confirmPassword;
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        console.log(errors.array());
+        return res.status(422).render('auth/signup', {
+            path: '/signup',
+            pageTitle: 'Signup',
+            errorMessage: errors.array()
+        });
+    }
 
     User.findOne({
         email: email
