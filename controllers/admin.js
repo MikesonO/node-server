c9fcbf0a0b28aac8e4ce2394b72543541a22299e
mike.osinowo <mikeson.jnr@gmail.com>
2024-03-27 17:22:08 +0000

    Add client side JS for product deletion

--- controllers/admin.js
+++ controllers/admin.js
@@ -180,8 +180,8 @@ exports.getProducts = (req, res, next) => {
         });
 };
 
-exports.postDeleteProduct = (req, res, next) => {
-    const prodId = req.body.productId;
+exports.deleteProduct = (req, res, next) => {
+    const prodId = req.params.productId;
     Product.findById(prodId)
         .then(product => {
             if (!product) {
@@ -191,12 +191,10 @@ exports.postDeleteProduct = (req, res, next) => {
             return Product.deleteOne({ _id: prodId, userId: req.user._id });
         })
         .then(() => {
-            console.log('Product deleted!')
-            res.redirect('/admin/product-list');
+            console.log('Product deleted!');
+            res.status(200).json({ message: "Success!" });
         })
         .catch(err => {
-            const error = new Error('Post Delete Product failed.')
-            error.httpStatusCode = 500;
-            return next(error);
+            res.status(500).json({ message: "Deleting product failed." });
         });
 };
